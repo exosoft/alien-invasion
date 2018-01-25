1471d9e522b18f7444cb12292a03038f6a1aaccc
Rick <exosoft@cyberspheric.com>
2018-01-25 13:41:18 -0500

    Move game settings out to thier own class.

---
+++ settings.py
@@ -0,0 +1,12 @@
+# source code file interation #1
+# project interation #3
+
+class Settings():
+		"""A class to store all settings for Alien Invasion."""
+		
+		def __init__(self):
+			""""Initialize the game's settings."""
+			# Screen settings
+			self.screen_width = 1200
+			self.screen_height = 800
+			self.bg_color = (230, 230, 230)
